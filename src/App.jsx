3471a37a57a68257bd7694f7d2b4482f5963f0e2
Alejandro M <alejandromaimus147@gmail.com>
2023-08-16 13:43:54 +0200

    turns icons and board behaviour

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-const TURNS = {
-  x: 'x',
-  o: 'o'
-}
+import { useState } from 'react'
 
-const board = Array(9).fill(null)
+const TURNS = { x: '✖️', o: '⭕' }
 
 const Square = ({ children, updateBoard, index }) => {
+  const handleClick = () => {
+    // cosas
+    updateBoard()
+  }
+
   return (
-    <div className='square'>
+    <div onClick={handleClick} className='square'>
       {children}
     </div>
   )
 }
 
+const initialBoard = Array(9).fill(null)
+const initialTurn = TURNS.x
+const mock = ['✖️', '✖️', '✖️', '✖️', '✖️', '⭕', '⭕', '⭕', '⭕']
+
 function App () {
+  const [board, setBoard] = useState(initialBoard)
+  const [turn, setTurn] = useState(initialTurn)
+
+  const updateBoard = (/* cosas */) => {
+    const newTurn = turn === TURNS.x ? TURNS.o : TURNS.x
+    setTurn(newTurn)
+  }
+
   return (
     <main className='board'>
       <h1>TicTacToe</h1>
@@ -24,12 +38,18 @@ function App () {
               <Square
                 key={index}
                 index={index}
+                updateBoard={updateBoard}
               >
-                {index}
+                {board[index]}
               </Square>
             )
           })
         }
+      </section>
+      <section className='turn'>
+        <h1>
+          {turn} Turn!
+        </h1>
       </section>
     </main>
   )
